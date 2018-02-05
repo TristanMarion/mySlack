7eb217a73b20c0ad0588dd32377afd2949af6bd6
Alexis Coquard <coquar_a@etna-alternance.net>
2018-02-05 21:22:14 +0100

    Add CLI arguments for client

--- includes/libmy.h
+++ includes/libmy.h
@@ -7,6 +7,6 @@ void my_putstr(const char *str);
 void my_put_nbr(int n);
 char *my_strdup(const char *str);
 int my_strcmp(const char *s1, const char *s2);
-int my_getnbr(char *str)
+int my_getnbr(char *str);
 
 #endif
