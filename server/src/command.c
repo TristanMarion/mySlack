cc97dc5a905e4981b2f563a29b774ae2af149f6c
Tristan Marion <trismarion@gmail.com>
2018-02-09 01:05:58 +0100

    Private message

--- server/src/command.c
+++ server/src/command.c
@@ -3,6 +3,7 @@
 const t_server_command server_command_array[] = {
     {"send_message", send_message},
     {"list_commands", list_commands},
+    {"direct_message", direct_message},
     {NULL, NULL}};
 
 void manage_message(t_server *server, t_client *client, char *message)
@@ -25,6 +26,12 @@ void manage_message(t_server *server, t_client *client, char *message)
     }
     response = my_strdup("Unknown command. Type /list_commands to show available commands.");
     send(client->fd_id, response, my_strlen(response), 0);
+}
+
+void send_error(t_client *client, char *error_message)
+{
+    send(client->fd_id, error_message, my_strlen(error_message), 0);
+    free(error_message);
 }
 
 void send_message(t_server *server, t_client *client, char **splitted_message)
@@ -50,8 +57,8 @@ void send_message(t_server *server, t_client *client, char **splitted_message)
 void list_commands(t_server *server, t_client *client, char **splitted_message)
 {
     // silence unused variables warnings.
-    (void) server;
-    (void) splitted_message;
+    (void)server;
+    (void)splitted_message;
     int i;
     int len;
     t_server_command current_command;
@@ -68,4 +75,47 @@ void list_commands(t_server *server, t_client *client, char **splitted_message)
         i++;
     }
     send(client->fd_id, all_commands, my_strlen(all_commands), 0);
+}
+
+void direct_message(t_server *server, t_client *client, char **splitted_message)
+{
+    t_client *current_client;
+    char **splitted_core_message;
+
+    splitted_core_message = parse_command(splitted_message[1], ' ');
+    current_client = server->clients_list->first_client;
+    if (splitted_core_message[0] == NULL || splitted_core_message[1] == NULL)
+    {
+        send_error(client, my_strdup("Usage : /direct_message <nickname> <message>"));
+        return;
+    }
+    if (my_strcmp(client->nickname, splitted_core_message[0]) == 0)
+    {
+        send_error(client, my_strdup("WHAT THE FUCK ARE YOU DOING"));
+        return;
+    }
+    while (current_client != NULL)
+    {
+        if (my_strcmp(current_client->nickname, splitted_core_message[0]) == 0)
+        {
+            // TODO : Use implode function to send the whole splitted_core_message except first element
+            send_direct_message(client, current_client->fd_id, splitted_core_message[1]);
+            return;
+        }
+        current_client = current_client->next;
+    }
+    send_error(client, my_strdup("User not found"));
+    return;
+}
+
+void send_direct_message(t_client *client, int target, char *message)
+{
+    int needed;
+    char *sent_message;
+
+    needed = snprintf(NULL, 0, "%s : %s", client->nickname, message) + 1;
+    sent_message = malloc(needed);
+    snprintf(sent_message, needed, "%s : %s", client->nickname, message);
+    send(target, sent_message, my_strlen(sent_message), 0);
+    free(sent_message);
 }
