52bd4b74029f6138f3356172a1c4664d23250105
Tristan Marion <trismarion@gmail.com>
2018-02-21 23:03:13 +0100

    Cutting main_loop in smaller functions

--- server/src/server.c
+++ server/src/server.c
@@ -2,7 +2,6 @@
 
 void main_loop(t_server *server)
 {
-    t_client *current_client;
     int max;
     char message[MAX_LEN];
     int end;
@@ -10,25 +9,12 @@ void main_loop(t_server *server)
     end = 0;
     while (end == 0)
     {
-        FD_ZERO(&(server->fds));
-        FD_SET(0, &(server->fds));
-        FD_SET(server->sockfd, &(server->fds));
-        max = server->sockfd;
-
-        current_client = server->clients_list->first_client;
-        while (current_client != NULL)
-        {
-            FD_SET(current_client->fd_id, &(server->fds));
-            max = current_client->fd_id > max ? current_client->fd_id : max;
-            current_client = current_client->next;
-        }
-
+        setup_loop(server, &max);
         if (select(max + 1, &(server->fds), NULL, NULL, NULL) < 0)
         {
             put_error("select()");
             return;
         }
-
         if (FD_ISSET(server->sockfd, &(server->fds)))
         {
             if (new_client(server) != 0)
@@ -39,33 +25,59 @@ void main_loop(t_server *server)
         }
 
         if (FD_ISSET(0, &(server->fds)))
-        {
-            read(0, message, MAX_LEN - 1);
-            {
-                char *p = NULL;
-                p = my_strstr(message, "\n");
-                if (p != NULL)
-                {
-                    *p = 0;
-                }
-                else
-                {
-                    message[MAX_LEN - 2] = '\n';
-                    message[MAX_LEN - 1] = 0;
-                }
-            }
-            before_manage_message(server, message, &end);
-        }
+            read_standard_input(server, message, &end);
+        clients_loop(server);
+    }
+}
 
-        current_client = server->clients_list->first_client;
-        while (current_client != NULL)
+void setup_loop(t_server *server, int *max)
+{
+    t_client *current_client;
+
+    FD_ZERO(&(server->fds));
+    FD_SET(0, &(server->fds));
+    FD_SET(server->sockfd, &(server->fds));
+    *max = server->sockfd;
+    current_client = server->clients_list->first_client;
+    while (current_client != NULL)
+    {
+        FD_SET(current_client->fd_id, &(server->fds));
+        *max = current_client->fd_id > *max ? current_client->fd_id : *max;
+        current_client = current_client->next;
+    }
+}
+
+void read_standard_input(t_server *server, char *message, int *end)
+{
+    read(0, message, MAX_LEN - 1);
+    {
+        char *p = NULL;
+        p = my_strstr(message, "\n");
+        if (p != NULL)
         {
-            if (FD_ISSET(current_client->fd_id, &(server->fds)))
-            {
-                poll_events(server, current_client);
-            }
-            current_client = current_client->next;
+            *p = 0;
         }
+        else
+        {
+            message[MAX_LEN - 2] = '\n';
+            message[MAX_LEN - 1] = 0;
+        }
+    }
+    before_manage_message(server, message, end);
+}
+
+void clients_loop(t_server *server)
+{
+    t_client *current_client;
+    
+    current_client = server->clients_list->first_client;
+    while (current_client != NULL)
+    {
+        if (FD_ISSET(current_client->fd_id, &(server->fds)))
+        {
+            poll_events(server, current_client);
+        }
+        current_client = current_client->next;
     }
 }
 
@@ -94,12 +106,12 @@ void disconnect(t_server *server, t_client *client)
 void before_manage_message(t_server *server, char *message, int *end)
 {
     char *command;
-	char *core_message;
+    char *core_message;
     char *sent_message;
-	
-	command = get_message_command(message);
-	core_message = get_core_message(message);
-	sent_message = generate_message(my_strdup("%s\037%s"), 1, command, core_message);
-	server_manage_message(server, end, sent_message);
-	free(sent_message);
+
+    command = get_message_command(message);
+    core_message = get_core_message(message);
+    sent_message = generate_message(my_strdup("%s\037%s"), 1, command, core_message);
+    server_manage_message(server, end, sent_message);
+    free(sent_message);
 }
