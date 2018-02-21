a5f51fb5eb08b578b68fee474b922d5cda26c84d
Tristan Marion <trismarion@gmail.com>
2018-02-21 23:32:35 +0100

    Move `manage_message` and delete `command.c`

--- server/src/server.c
+++ server/src/server.c
@@ -115,3 +115,19 @@ void before_manage_message(t_server *server, char *message, int *end)
     server_manage_message(server, end, sent_message);
     free(sent_message);
 }
+
+void manage_message(t_server *server, t_client *client, char *message)
+{
+    char **splitted_message;
+    const t_client_command *command;
+    char *response;
+
+    splitted_message = parse_command(message, '\037');
+    if ((command = get_command(splitted_message[0])) != NULL)
+    {
+        command->cmd_ptr(server, client, splitted_message);
+        return;
+    }
+    response = my_strdup("Unknown command. Type /list_commands to show available commands.");
+    send_special(client, my_strdup("error"), response);
+}
